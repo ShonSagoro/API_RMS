40f741a090f398247b47eb3be993c21e84079e06
ShonSagoro <ramosproque@gmail.com>
2022-10-02 19:20:34 -0500

    first commit

---
+++ src/main/java/com/examplerm/rmdemo/RmdemoApplication.java
@@ -0,0 +1,13 @@
+package com.examplerm.rmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplerm/rmdemo/RmdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplerm.rmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
